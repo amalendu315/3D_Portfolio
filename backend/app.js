e0a18ba8c8d65fa7119d15307401443abb8908de
Amalendu Pandey <amalendupandey56@gmail.com>
2022-09-28 16:22:48 +0530

    fixed

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { userRouter } from "./routes/User.js";
 
 export const app = express();
@@ -9,3 +10,9 @@ app.use(express.urlencoded({ extended: true, limit:"50mb" }));
 app.use(cookieParser());
 
 app.use("/api", userRouter);
+
+app.use(express.static(path.resolve("./frontend/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve("./frontend/build/index.html"));
+});
